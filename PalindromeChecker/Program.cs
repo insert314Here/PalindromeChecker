56bcbb136437ff636820b36388e693b43b5ef461
insert314Here <89361627+insert314Here@users.noreply.github.com>
2021-12-02 17:40:47 -0500

    Add project files.

---
+++ PalindromeChecker/Program.cs
@@ -0,0 +1,18 @@
+﻿//read console line to get a word to check
+// convert to char array
+//reverse the word
+//match case
+
+//is the original string the same value as the reveresed string
+string str = Console.ReadLine();
+char[] charMessage = str.ToCharArray();
+Array.Reverse(charMessage);
+
+int x = 0;
+// count the o's
+foreach (char i in charMessage) { if (i == 'o') { x++; } }
+// convert it back to a string
+string new_message = new String(charMessage);
+// print it out
+Console.WriteLine(new_message);
+Console.WriteLine($"'o' appears {x} times.");
