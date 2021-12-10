0f64edc50e5b2d54f7d5604ea0fef65694c3570f
insert314Here <89361627+insert314Here@users.noreply.github.com>
2021-12-10 11:01:23 -0500

    completed

--- PalindromeChecker/Program.cs
+++ PalindromeChecker/Program.cs
@@ -1,18 +1,45 @@
-﻿//read console line to get a word to check
-// convert to char array
-//reverse the word
-//match case
+﻿Console.WriteLine("Welcome to the palindrome checker.\n Enter a word:");
+string word = Convert.ToString(Console.ReadLine());
+word = word.ToLower(); //converting to all lower case to make vowel checker easier to code
 
-//is the original string the same value as the reveresed string
-string str = Console.ReadLine();
-char[] charMessage = str.ToCharArray();
-Array.Reverse(charMessage);
+//initializing two char lists for future comparison
+char[] charWord = word.ToCharArray();
+char[] charWord2 = word.ToCharArray();
+Array.Reverse(charWord2);
+string stringWord2 = "";
+foreach (char c in charWord2)
+{
+    stringWord2 += c;
+}
 
-int x = 0;
-// count the o's
-foreach (char i in charMessage) { if (i == 'o') { x++; } }
-// convert it back to a string
-string new_message = new String(charMessage);
-// print it out
-Console.WriteLine(new_message);
-Console.WriteLine($"'o' appears {x} times.");
+//for loop to check if each cahracter matches between 2 lists
+for (int i = 0; i < word.Length; i++)
+{
+    if (charWord[i] != charWord2[i])
+    {
+        Console.WriteLine("Looks like this is not a palindrome.\n");
+        Console.WriteLine($"{word} is not the same as {stringWord2}");
+        break;
+    }
+    else //(charWord[i] == charWord2[i])
+    {
+        //scan for vowels and output ratio of values in the word
+        int vowelCount = 0;
+        foreach (char v in charWord)
+        {
+            if (v == 'a' || v == 'e' || v == 'i' || v == 'o' || v == 'u')
+            {
+                vowelCount++;
+            }
+        }
+        Console.WriteLine("Congrats, you entered a palindrome.");
+        Console.WriteLine($"{word} is the same as {stringWord2} and has {vowelCount} vowels");
+        break;
+    }
+}
+
+
+
+Console.ReadKey();
+
+
